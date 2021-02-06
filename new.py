f0d10466ea8d6d4c40a8a4a0fd31cdf585458f15
cedcoss <cedcoss@jishnu.kapoor>
2021-02-06 15:32:12 +0530

    initial

---
+++ new.py
@@ -0,0 +1,15 @@
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+<<<<<<< HEAD
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+=======
+print("\n\nRecursion Example Results yeahhhhhhhhh")
+tri_recursion(6)
+
